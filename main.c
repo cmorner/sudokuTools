2f8464dcd412b63aa05bcbae21ead0c8b69a35ce
Carl <carlmorner@hotmail.com>
2017-05-31 00:33:05 +0200

    checking sudoku solution functionality complete

--- main.c
+++ main.c
@@ -4,8 +4,18 @@ void presentProgram();
 void loadSudokuFromFileToArray(int sudokuArray[9][9], char path[255]);
 void presentEnteredSudoku(int sudokuArray[9][9]);
 void createNumberedArray(int array1[]);
-int legitSudokuSolution(int sudokuarray[9][9]);
+
+// Utilities
+void getBlockFromPuzzleByIndex(int sudokuArray[9][9], int block[9], int blockIndex);
+//void getRowFromPuzzleByIndex(int sudokuArray[9][9])
 int allNumbersInArray(int array[]);
+int digitIsInArray(int digits[9], int digit);
+
+// Checking sudoku solution
+int legitSudokuSolution(int sudokuarray[9][9]);
+
+// Solving sudoku
+void solveSudoku(int sudokuArray[9][9])
 
 
 int main()
@@ -29,6 +39,8 @@ int main()
     **/
 
     presentEnteredSudoku(sudokuArray);
+
+    solveSudoku(sudokuArray);
 
     legitSudokuSolution(sudokuArray);
 
@@ -104,9 +116,65 @@ int allNumbersInArray(int array[]) {
             return 0;
         }
     }
-
+    //printf("\t");
 
     return 1;
+}
+
+void getBlockFromPuzzleByIndex(int sudokuArray[9][9], int block[9], int blockIndex) {
+    int r, c, n, x;
+
+    // Set row and column starting values depending on which
+    // block to get
+    switch (blockIndex) {
+        case 0 :
+            r = 0;
+            c = 0;
+            break;
+        case 1 :
+            r = 0;
+            c = 3;
+            break;
+        case 2 :
+            r = 0;
+            c = 6;
+            break;
+        case 3 :
+            r = 3;
+            c = 0;
+            break;
+        case 4 :
+            r = 3;
+            c = 3;
+            break;
+        case 5 :
+            r = 3;
+            c = 6;
+            break;
+        case 6 :
+            r = 6;
+            c = 0;
+            break;
+        case 7 :
+            r = 6;
+            c = 3;
+            break;
+        case 8 :
+            r = 6;
+            c = 6;
+            break;
+    }
+
+
+
+    // Get the values from the block that spans over 3 rows
+    // And add that to the block array
+    for (n=0; n<3; n++) {
+        // Iterate over row columns
+        for (x=0; x<3; x++) {
+            block[n*3+x] = sudokuArray[r+n][c+x];
+        }
+    }
 }
 
 int legitSudokuSolution(int sudokuArray[9][9]) {
@@ -121,7 +189,7 @@ int legitSudokuSolution(int sudokuArray[9][9]) {
             return 0;
         }
     }
-    printf("all rows ok!\n");
+    //printf("all rows ok!\n");
 
     // Check that all columns include correct numbers
     for (c=0; c<9; c++) {
@@ -130,42 +198,28 @@ int legitSudokuSolution(int sudokuArray[9][9]) {
         }
         if (allNumbersInArray(column_array) != 1) {
             printf("Faulty column nr: %i \n", c+1);
+            return 0;
         }
     }
-    printf("all columns ok!\n");
+    //printf("all columns ok!\n");
 
-    // Check that each block holds the correct numbers
+
     for (r=0; r<9; r++) {
-        // Add one number to each block
-        if (r < 3) {
-            y = 0;
-        } else if (2 < r < 6) {
-            y = 3;
-        } else if (5 < r < 9) {
-            y = 6;
-        }
-        for (c=0; c<9; c++) {
-            if (c < 3) {
-                x = 0;
-            } else if (2 < c < 6) {
-                x = 1;
-            } else if (5 < c < 9) {
-                x = 2;
-            }
-
-
-            blocks[y+x][(c%3)+(r%3)*3] = sudokuArray[r][c];
-        }
+        getBlockFromPuzzleByIndex(sudokuArray, blocks[r], r);
     }
 
-    getchar();
-    //printf("block 1: %i", blocks[][]);
+    //printf("blocks: \n");
     for (c=0; c<9; c++) {
-        printf(" %i", blocks[3][c]);
-        //if (allNumbersInArray(blocks) != 1) {
-        //    printf("Faulty block nr: %i \n", c+1);
-        //}
+        for (r=0; r<9; r++) {
+            printf("%i", blocks[c][r]);
+        }
+        if (allNumbersInArray(blocks[c]) != 1) {
+            printf("Faulty block index: %i \n", c);
+            return 0;
+        }
+        //printf("\t");
     }
+    //printf("All blocks ok!\n");
 
     return 1;
     // Kolla att alla rader innehåller siffrorna 1-9
@@ -173,4 +227,41 @@ int legitSudokuSolution(int sudokuArray[9][9]) {
     // Kolla att alla boxarna
 }
 
+int digitIsInArray(int digits[9], int digit) {
+    int c;
+    for (c=0; c<9; c++) {
+        if (digits[c] == digit) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
+// Skulle kanske kunna återanvända digit in array och hämta varje rad block och kolumn
+void getDigitFrequencyCount(int sudokuArray[9][9], digitFrequencyCount[9]) {
+    int r, c;
+
+    for ()
+}
+
+void solveSudoku(int sudokuArray[9][9]) {
+    // Kanske implementera en stack struktur för att kunna lägga på vad som
+    // skall göras härnäst
+
+    // Ha någon typ av timeout
+    // Scannar av arrayen och ser vilka siffror som det finns flest av
+    // och försöker lösa någon av dom
+    // Lösning utan något optimerings tänk och lösning med optimeringstänk
+    // Dvs strategiska beslut som avgör t.ex vilken siffra man vill lösa efter först
+
+    //
+
+    int digitFrequencyCount[9];
+
+
+
+}
+
+
+// C klasser, pointers
+// Borde funktioner returnera något när det gäller arrayer
